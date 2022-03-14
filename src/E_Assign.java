245456fb631b3b6b07f1f946a2bc851735481536
rol <shahrulmubarak2020@gmail.com>
2022-03-14 16:18:29 +0700

    memberikan nilai ke suatu tipe data

---
+++ src/E_Assign.java
@@ -0,0 +1,23 @@
+public class E_Assign {
+    public static void main(String[] args) {
+        boolean b = true;
+        System.out.println("Value b= " + b);
+        char c = 'C';
+        System.out.println("Value c= " + c);
+        byte bt = 10;
+        System.out.printf("Value bt= " + bt);
+        short s = 20;
+        System.out.println("Value s= " + s);
+        int i = 30;
+        System.out.println("Value i= " + i);
+        long L = 40;
+        System.out.println("Value L= " + L);
+        float f = 3.14f;
+        System.out.println("Value f= " + f);
+        double d = 3.14;
+        System.out.println("Value d= " + d);
+    }
+}
+
+
+
