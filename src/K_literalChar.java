b2b2efb58a5c8e6ea299e8b70b83c26000c2f2d5
rol <shahrulmubarak2020@gmail.com>
2022-03-14 17:20:10 +0700

    MENCETAK LITERAL CHARACTER

---
+++ src/K_literalChar.java
@@ -0,0 +1,18 @@
+public class K_literalChar {
+    public static void main(String[] args) {
+        {
+            char tab = '\t';
+            char newline = '\n';
+            char singleQuote = '\'';
+            char doubleQuote = '\"';
+            System.out.println("ini contoh" + "literal tab");
+            System.out.println(" ini contoh" + newline + "literal newline");
+            System.out.println("" + singleQuote + "ini contoh literal single" + singleQuote);
+            System.out.println("" + doubleQuote + "ini contoh literal double quote" + doubleQuote);
+
+
+        }
+    }
+}
+
+
