2ff028e049374476b4a64c14040f468b42907e91
rol <shahrulmubarak2020@gmail.com>
2022-03-14 15:08:10 +0700

    project pertemuan 1 dasar java

---
+++ src/A_blokDankomentar.java
@@ -0,0 +1,15 @@
+public class A_blokDankomentar {
+    public static void main(String[] args) {
+        System.out.println("pemrongraman berorientasi objek dengan java");
+        //contoh komentar 1 baris
+        /*
+        contoh komentar lebih dari satu baris
+         /*
+
+
+
+         /**
+         java doc---> baris ini untuk mendukumentasikan kode prongram
+         */
+    }
+}
